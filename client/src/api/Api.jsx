6cef8820bf9fec2e77703f46db98908019a7d28e
Logan <logansailer0633@gamil.com>
2025-02-26 14:09:52 -0500

    backend for Render

--- client/src/api/Api.jsx
+++ client/src/api/Api.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export async function productsData() {
-  const products = await axios.get("http://localhost:8000/api/products");
+  const products = await axios.get(
+    "https://ecommerce-backend-rouu.onrender.com/api/products"
+  ); // Fetching data from the backend (replace with "http://localhost:8000/api/products" if running locally)
   return products;
 }
